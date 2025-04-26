a90fb753938202fb9f87ae8082b984b01f1b3d79
juliidiiaz04 <diazjulietacamila@gmail.com>
2025-04-26 20:58:41 -0300

    tp2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Main from "./componentes/Main";
-import Nosotros from "./componentes/Nosotros";
 import Ju from "./Imagen/Ju.png"
 
 
@@ -11,18 +10,11 @@ function App() {
       legajo: "61472",
       github: "https://github.com/juliidiiaz04",
       foto:Ju,
-    },
-    {
-      nombre: "Esteban Maximiliano",
-      apellido: "Samaniego",
-      legajo: "61468",
-      github: "https://github.com/MaxiSama12",
-      foto: "",
     }];
   return (
     <>
       <Main integrantes={integrantes} />
-      <Nosotros integrantes={integrantes} />
+      
     </>
   );
 }
